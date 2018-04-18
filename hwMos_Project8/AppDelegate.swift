22d03700469ed5e9122a1ba68b832d0087f6e22a
Kyle Absten <kyleabsten@gmail.com>
2018-04-18 10:03:17 -0700

    Initial Commit

---
+++ hwMos_Project8/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  hwMos_Project8
+//
+//  Created by Kyle Absten on 4/18/18.
+//  Copyright © 2018 Kyle Absten. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ hwMos_Project8/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hwMos_Project8
+//
+//  Created by Kyle Absten on 4/18/18.
+//  Copyright © 2018 Kyle Absten. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
